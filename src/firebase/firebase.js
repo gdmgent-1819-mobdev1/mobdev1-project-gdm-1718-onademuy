7e1ffb5e8e53f10d6d1552ed3a3c92c6823a478f
Ona Demuytere <onademuy@student.arteveldehs.be>
2018-11-22 13:49:54 +0100

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ const firebaseInstance = require('firebase');
 // Initialize Firebase
 // TODO: Replace with your project's config
 const config = {
-  apiKey: '<key>',
-  authDomain: '<projectId>.firebaseapp.com',
-  databaseURL: 'https://<projectId>.firebaseio.com',
-  projectId: '<projectId>',
-  storageBucket: '<projectId>.appspot.com',
-  messagingSenderId: '52666332181',
+  apiKey: 'AIzaSyDerVPpCIo6EovKT5qxEI5bkaWSb7BN7rY',
+  authDomain: 'eindopdrachtwebdev.firebaseapp.com',
+  databaseURL: 'https://eindopdrachtwebdev.firebaseio.com',
+  projectId: 'eindopdrachtwebdev',
+  storageBucket: 'eindopdrachtwebdev.appspot.com',
+  messagingSenderId: '190328315642',
 };
 
 let instance = null;
